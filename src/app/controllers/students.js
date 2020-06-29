f34010784e9a27647a3c486c03b8daf685dea621
odairdev <odair.dev@gmail.com>
2020-06-29 12:21:36 -0300

    All CRUD operations working with PostgreSQL db.

--- src/app/controllers/students.js
+++ src/app/controllers/students.js
@@ -1,32 +1,54 @@
 const { age, grade, date } = require('../../lib/utils')
+const Student  = require('../models/student')
 
 module.exports = {
     index(req, res) {
-        return
+        Student.all(function(students) {
+            res.render('students/index', { students })
+        })
     },
 
     show(req, res) {
-        return
+        Student.find(req.params.id, function(student) {
+            if(!student) res.send("Student not found")
+
+            student.grade = grade(student.grade)
+            student.age = age(student.birth)
+
+            res.render('students/show', { student })
+        })
     },
 
     edit(req, res) {
-        return
+        Student.find(req.params.id, function(student) {
+            if(!student) res.send("Student not found")
+
+            student.birth = date(student.birth).iso
+
+            res.render(`students/edit`, { student })
+        })
     },
 
     create(req, res) {
-        return
+        return res.render('students/create')
     },
 
     post(req, res) {
-        return
+        Student.create(req.body, function(student) {
+            res.redirect(`/students/${student.id}`)
+        })
 
     },
 
     put(req, res) {
-        return
+        Student.update(req.body, function() {
+            res.redirect(`/students/${req.body.id}`)
+        })
     },
 
     delete(req, res) {
-        return
+        Student.delete(req.body.id, function() {
+            res.redirect('/students')
+        })
     }
 }
